9e7b8cffcf8909ae3f72cd402fa93c7e52db201a
pcutting <phil@nevie.com>
2020-10-14 00:10:52 +0300

    video 168

--- app/src/main/java/com/samples/bootcamp/udemyflikrapp/PhotoDetailsActivity.kt
+++ app/src/main/java/com/samples/bootcamp/udemyflikrapp/PhotoDetailsActivity.kt
@@ -18,9 +18,13 @@ class PhotoDetailsActivity : BaseActivity() {
 
         val photo = intent.getParcelableExtra<Photo>(PHOTO_TRANSER) as Photo
             //intent.getSerializableExtra(PHOTO_TRANSER) as Photo
-        photo_title.text = photo.title
-        photo_tags.text = photo.tags
-        photo_author.text = photo.author
+//        photo_title.text = "Title:" + photo.title
+//        photo_tags.text = "Tags:" + photo.tags
+        photo_title.text = resources.getString(R.string.photo_title_text, photo.title)
+        photo_tags.text = resources.getString(R.string.photo_tags_text, photo.tags)
+
+        //photo_author.text = photo.author
+        photo_author.text = resources.getString(R.string.photo_author_text, photo.author)
 
         Picasso.with(this).load(photo.link)
             .error(R.drawable.image_holder_48)
